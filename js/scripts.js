2833e88fd9d08c2a10e002399db944b0838d4dbe
paul <pmorin35@gmail.com>
2015-06-28 15:11:40 -0700

    working exercise on dom manipulation

---
+++ js/scripts.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+  $("button#hello").click(function(){
+    $("ul#user").prepend("<li>Hello!</li>")
+    $("ul#webpage").prepend("<li>Why hello there!</li>")
+
+    $("ul#user").children("li").first().click(function(){
+      $(this).remove();
+    });
+
+    $("ul#webpage").children("li").first().click(function(){
+      $(this).remove();
+    });
+  });
+
+  $("button#goodbye").click(function(){
+    $("ul#user").prepend("<li>Goodbye!</li>")
+    $("ul#webpage").prepend("<li>Goodbye, buddy!</li>")
+
+    $("ul#user").children("li").first().click(function(){
+      $(this).remove();
+    });
+
+    $("ul#webpage").children("li").first().click(function(){
+      $(this).remove();
+    });
+  });
+
+  $("button#stop").click(function(){
+    $("ul#user").prepend("<li>Stop copying me!</li>")
+    $("ul#webpage").prepend("<li>Pardon me!</li>")
+
+    $("ul#user").children("li").first().click(function(){
+      $(this).remove();
+    });
+
+    $("ul#webpage").children("li").first().click(function(){
+      $(this).remove();
+    });
+  });
+});
